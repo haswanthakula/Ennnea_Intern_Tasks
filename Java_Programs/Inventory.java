0129774e68a3ba641ce4570fd63774709c1b1fb0
Haswanth Akula <haswanthakula5@gmail.com>
2024-12-07 10:12:33 +0530

    Create Inventory.java

---
+++ Java_Programs/Inventory.java
@@ -0,0 +1,37 @@
+{
+    private int Id;
+    private String name;
+    private double price;
+
+    public Inventory(int id, String name, double price) {
+        Id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+
+}
